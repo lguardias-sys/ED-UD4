f25046293916d3330ff9df6508f736bf49019905
Oscar <oubedaf@iesch.org>
2025-10-10 21:13:38 +0200

    hola

--- src/Ej.java
+++ src/Ej.java
@@ -3,5 +3,9 @@ public class Ej {
         //Mi primer Hola mundo
 
         System.out.println("Hola mundo");
+
+        for (int i = 0; i < 101; i++) {
+            System.out.println("Hola mundo");
+        }
     }
 }
