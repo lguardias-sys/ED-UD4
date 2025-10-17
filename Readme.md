d0dbd505e7cf4be7994bf45a4041d71a3cdeb287
LJ Guardia <lguardias@iesch.org>
2025-10-17 21:13:03 +0200

    Primer commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+## Entornos de Desarrollo. UD4 Optimización y Documentación
+Proyecto Java para aprender a utilizar Git.
+Luis J Guardia
---
+++ src/ed/ud4/Average.java
@@ -0,0 +1,7 @@
+package ed.ud4;
+import java.math.*;
+
+public class Average {
+    double a = Math.random() * 19;
+    int a1 = (int) a;
+}
